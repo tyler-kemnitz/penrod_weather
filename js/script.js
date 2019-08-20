7e3a39ecca124b62f9ba7cd1021066a78607129d
Jonah <contact@jonahjahns.com>
2019-08-20 17:46:32 -0500

    finished basic card design and toggling feature

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.loc-card').click(function() {
+        $('#fullscreen').show('slow');
+    });
+    $('#exit').click(function() {
+        $('#fullscreen').hide('slow');
+    });
+});

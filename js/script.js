e7768e78e2a706c81626783534d1a0fd78c1a462
Jonah <contact@jonahjahns.com>
2019-08-22 23:55:19 -0500

    Finished API calls for hourly report and most styling for site components

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
 var api_cities = "5263058,4887398,5037649,4684888"; //mke, chi, minn, dallas
 var app_id = "923e44385481ced80694c62dda9823b1";
-
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+'Thursday', 'Friday', 'Saturday'];
+const MONTHS = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
 
 function get_day_name(y, m, d) {
     //returns day of week based on date given
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-    'Thursday', 'Friday', 'Saturday'];
     var res = new Date(y, --m, d);
-    return res && days[res.getDay()];
+    return res && DAYS[res.getDay()];
 }
 
 function get_month_name(y, m, d) {
     //returns month of year based on date given
-    var months = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
     res = new Date(y, --m, d);
-    return res && months[res.getMonth()];
+    return res && MONTHS[res.getMonth()];
+}
+
+function convert_html_timestamp(clicked_date) {
+    /*converts time from clicked date block into a format
+    that can be compared to OpenWeatherMap's dt_text variable;
+    helper function for get_hourly_report*/
+    clicked_date = clicked_date.replace(",", "");
+    var date_array = clicked_date.split(" ");
+    //num.toString().padStart(4, "0");
+    var month_num = MONTHS.indexOf(date_array[0]) + 1;
+    var month_str = month_num.toString().padStart(2,"0");
+    var day_str = date_array[1].padStart(2,"0");
+    var res = date_array[2] + "-" + month_str + "-" + day_str;
+    return res;
+}
+
+function parse_display_temp(temp) {
+    //Truncates temperature from OpenWeatherMap API if needed
+    var res = temp;
+    if (res.includes('.')) {
+        res = res.substr(0, res.indexOf('.'));
+    }
+    res += (String.fromCharCode(176) + "F");
+    return res;
 }
 
 function translate_time_stamp(time) {
@@ -30,74 +53,25 @@ function translate_time_stamp(time) {
     return res;
 }
 
-function display_weekdays() {
-    //Displays next five days from current date
-    var today = new Date();
-    for (i=0; i < 5; i++) {
-        next_day = new Date(today);
-        next_day.setDate(today.getDate() + i);
+function military_to_standard(time) {
+    //converts military time to standard, returning a string
+    var res;
+    var time_arr = time.split(':');
+    var hours = Number(time_arr[0]);
+    var minutes = Number(time_arr[1]);
 
-        var day = String(next_day.getDate()).padStart(2, '0');
-        var month = String(next_day.getMonth() + 1).padStart(2, '0');
-        var year = next_day.getFullYear();
-
-        var $display_day = get_day_name(year, month, day);
-        var $display_month = get_month_name(year, month, day);
-
-        //set up html elements to be added to page
-        var $date_block = $("<div class='slds-col slds-grid slds-grid_vertical slds-grid_vertical-align-center date-block'></div>");
-        var $date_col = $("<div class='slds-col slds-text-heading_medium date'></div>");
-        var $weekday_p = $("<p class='weekday'>" + $display_day + "</p>");
-        var $date_p = $("<p class='date'>" + $display_month + " " + day + ", " + year + "</p>");
-        //update page
-        $('#five-day').append($date_block);
-        $($date_block).append($date_col);
-        $($date_col).append($weekday_p, $date_p);
+    if (hours > 0 && hours <= 12) {
+        res = "" + hours;
+    } else if (hours > 12) {
+        res = "" + (hours-12);
+    } else if (hours == 0) {
+        res = "12";
     }
-} 
 
-function get_curr_temp(json) {
-    for (i=0; i < json.list.length; i++) {
-        //get current temp for each city on initial display
-        var city_id = json.list[i].id;
-        //TODO::Need to check if full_high or low doesn't have decimcal before parsing further
-        var full_high = String(json.list[i].main.temp_max);
-        var full_low = String(json.list[i].main.temp_min);
-        var display_high = full_high.substr(0, full_high.indexOf('.')) + String.fromCharCode(176) + "F";
-        var display_low = full_low.substr(0, full_low.indexOf('.')) + String.fromCharCode(176) + "F";
-        var $display_temp = display_high + " / " + display_low;
-        //determine weather icon based on weather description and time
-        var weather_desc = json.list[i].weather[0].id;
-        var time_stamp = translate_time_stamp(json.list[i].dt);
-        var $img_src = determine_weather_icon(weather_desc, time_stamp);
-        //update page
-        //document.getElementById(city_id).innerHTML = display_temp;
-        $('img.'+city_id).attr('src', $img_src);
-        $('span.'+city_id).text($display_temp);
-    }        
-}
+    res += (minutes < 10) ? ":0" + minutes : ":" + minutes; //get minutes
+    res += (hours >= 12) ? " P.M." : " A.M.";  //get AM/PM
 
-function get_five_day(json) {
-    //Parses JSON data from OpenWeatherMap API to update website
-    for (i=0; i < json.list.length; i++) {
-        //separate date and time to determine weather icon later
-        var time_stamp = json.list[i].dt_text;
-        time_stamp = time_stamp.split(" "); 
-        var day_mm_year = time_stamp[0];
-        var time = time_stamp[1];
-        //get number of day to check for day blocks on site
-        var day_num = day_mm_year.substr(day_mm_year.lastIndexOf('-') + 1);
-        //split year, month, day to get Day of week for site
-        var date_array = day_mm_year.split('-');
-        var day = get_day_name(date_array[0], date_array[1], date_array[2]);
-        //if condition somewhere
-        //get high and low temperature for the day, as well as desc. for weather
-        low_temp = String(json.list[i].main.temp_min);
-        high_temp = String(json.list[i].main.temp_max);
-        display_high = high_temp.substr(0, high_temp.indexOf('.')) + "&#8457;";
-        display_low = low_temp.substr(0, low_temp.indexOf('.')) + "&#8457;";
-        weather_desc = json.list[i].weather.id;
-    }
+    return res;
 }
 
 function determine_weather_icon(desc, time) {
@@ -130,22 +104,106 @@ function determine_weather_icon(desc, time) {
     }
 }
 
-function update_five_day_report(high, low, desc, day, time) {
-    //Adds HTML given data for a particular day of the week
-    var $weather_block = $("<div id='weather-block' class='slds-col slds-grid slds-grid_vertical slds-grid_vertical-align_center'>Test Block</div>");
-    var $day_col = $("<div class= 'slds-col slds-text-heading_medium weather-day'>" + day + "</div> ");
+function display_weekdays(city_id) {
+    //Displays next five days from current date
+    var today = new Date();
+    for (i=0; i < 5; i++) {
+        next_day = new Date(today);
+        next_day.setDate(today.getDate() + i);
+
+        var day = String(next_day.getDate()).padStart(2, '0');
+        var month = String(next_day.getMonth() + 1).padStart(2, '0');
+        var year = next_day.getFullYear();
+
+        var $display_day = get_day_name(year, month, day);
+        var $display_month = get_month_name(year, month, day);
+
+        //set up html elements to be added to page
+        var $date_block = $("<div class='slds-col slds-grid slds-size_1-of-5 slds-grid_vertical slds-grid_vertical-align-center date-block'></div>");
+        var $date_col = $("<div class='slds-col slds-text-heading_medium date'></div>");
+        var $weekday_p = $("<p class='weekday'>" + $display_day + "</p>");
+        var $date_p = $("<p class='date'>" + $display_month + " " + day + ", " + year + "</p>");
+        var $city_identifier = $(" <input type='hidden' id='city-identifier' name='city-identifier' value=" + city_id + "></input");
+        //update page
+        $('#five-day').append($date_block);
+        $($date_block).append($date_col);
+        $($date_col).append($weekday_p, $date_p, $city_identifier);
+    }
+} 
+
+function get_curr_temp(json) {
+    for (i=0; i < json.list.length; i++) {
+        //get current temp for each city on initial display
+        var city_id = json.list[i].id;
+        //TODO::Need to check if full_high or low doesn't have decimcal before parsing further
+        var full_high = String(json.list[i].main.temp_max);
+        var full_low = String(json.list[i].main.temp_min);
+        var display_high = parse_display_temp(full_high);
+        var display_low = parse_display_temp(full_low);
+        var $display_temp = display_high + " / " + display_low;
+        //determine weather icon based on weather description and time
+        var weather_desc = json.list[i].weather[0].id;
+        var time_stamp = translate_time_stamp(json.list[i].dt);
+        var $img_src = determine_weather_icon(weather_desc, time_stamp);
+        //get sunrise and sunset times to display
+        var sunrise_timestamp = json.list[i].sys.sunrise;
+        var sunset_timestamp = json.list[i].sys.sunset;
+        var display_sunrise = military_to_standard(translate_time_stamp(sunrise_timestamp));
+        var display_sunset = military_to_standard(translate_time_stamp(sunset_timestamp));
+        //store html elements for sunrise/sunset to append to page's div
+        var $sunrise = (" <p class='rise-set'>SUNRISE: " + display_sunrise + "</p>");
+        var $sunset = (" <p class='rise-set'>SUNSET: " + display_sunset + "</p>");
+        //update page
+        $('img.'+city_id).attr('src', $img_src);
+        $('span.'+city_id).text($display_temp);
+        $('div.'+city_id).append($sunrise, $sunset);
+    }        
+}
+
+function get_hourly_report(json, date) {
+    //Parses JSON data from OpenWeatherMap API to update website for hourly report
+    var block_count = 0;
+    for (i=0; i < json.list.length; i++) {
+        //separate date and time to determine weather icon later
+        var time_stamp = json.list[i].dt_txt;
+        time_arr = time_stamp.split(" ");
+        var year_month_day = time_arr[0];
+        var hour_min_sec = time_arr[1];
+        if (year_month_day == date) {
+            var hour = time_stamp.substr(time_stamp.indexOf(" ")+1);
+            //get high and low temperature for the day
+            var temp = String(json.list[i].main.temp);
+            var display_temp = parse_display_temp(temp);
+            //determine weather icon based on weather description and time
+            var weather_desc = json.list[i].weather[0].id;
+            //update page
+            update_hourly_report(display_temp, weather_desc, military_to_standard(hour_min_sec));
+            block_count += 1;
+        }
+    }
+    //if no data was found for a particular day, update page to let user know
+    if (block_count == 0) {
+        $no_data = (" <p class='sorry-msg'>Apologies; Currently no weather available for this day.</p> ");
+        $('#hour-head').append($no_data);
+    } else $('#hour-head').text("");
+}
+
+function update_hourly_report(temp, desc, time) {
+    //Adds HTML for hourly weather given data for a particular day of the week
+    var $weather_block = $("<div class='weather-block slds-col slds-grid slds-grid_vertical'></div>");
+    var $day_col = $("<div class= 'slds-col slds-text-align_center weather-time'>" + time + "</div> ");
     //determine img src for weather icon and add appropriate image
     var $img_src = determine_weather_icon(desc, time);
-    //var $icon_col = $("<img class='forecast-icon' src='css/assets/images/rain.png' /> ");
+    var $temp_block = $(" <div class='slds-col slds-grid slds-grid_align-center'></div> ");
+    var $icon_block = $(" <div class='forecast-icon-container'></div> ");
     var $icon_col = $("<img class='forecast-icon' src=" + $img_src + " /> ");
-    var $high_low_col = $(" <div class='slds-col slds-text-heading_small'></div> ");
-    var $high_temp = $(" <span class='weather-high'>" + high + " / </span> ");
-    var $low_temp = $(" <span class='weather-low'>" + low + "</span> ");
+    var $high_low = $(" <span class='weather-hl'>" + temp + "</span> ");
 
     //append to five-day weather report sequentially
-    $("#five-day").append($weather_block);
-    $($weather_block).append($day_col, $icon_col, $high_low_col);
-    $($high_low_col).append($high_temp, $low_temp);
+    $("#hourly-report").append($weather_block);
+    $($weather_block).append($day_col, $temp_block);
+    $($temp_block).append($icon_block, $high_low);
+    $($icon_block).append($icon_col);
 }
 
 $(document).ready(function() {
@@ -166,19 +224,52 @@ $(document).ready(function() {
         var $city_state = $(this).find('h2.location').text();
         var $temp = $(this).find('span.temp').text();
         var $curr_img_src = $(this).find('img').attr('src');
+        //grab city ID and store in weekday blocks to use for AJAX request later
+        var $curr_city = $(this).find('img').attr('class');
         $('#city-state').text($city_state);
         $('#current-temp').text($temp);
         $('#curr-icon').attr('src', $curr_img_src);
         //populate weekday blocks
         if ($('#five-day').is(':empty')) {
-            display_weekdays();
+            display_weekdays($curr_city);
         }
         $('#transparent').css("opacity", "0.6");
         $('#fullscreen').show('slow');
+        $('#five-day').show('slow');
     });
 
     $('#exit').click(function() {
         $('#fullscreen').hide('slow');
+        $('#hourly-report').hide();
         $('#transparent').css("opacity", "1");
+    });
+
+    $(document).on("click", "div.date", function() {
+        var clicked_date = $(this).find('p.date').text();
+        var converted_timestamp = convert_html_timestamp(clicked_date);
+        var city_id = $(this).find('input#city-identifier').val();
+        $.ajax({
+            type: "POST",
+            url: "http://api.openweathermap.org/data/2.5/forecast?id="+city_id+"&appid="+app_id+"&units=imperial",
+            dataType: "json",
+            success: function(result, status, xhr) {
+                //update date header
+                $('#hourly-date').html(clicked_date);   
+                //clear contents from any previous user clicks before displaying new data
+                $('.weather-block').remove()
+                get_hourly_report(result, converted_timestamp);
+                //switch user views
+                $('#five-day').hide('slow');
+                $('#hourly-report').show('slow');
+            },
+            error: function(xhr, status, error) {
+                console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        })
+    });
+
+    $('#hourly-exit').click(function() {
+        $('#hourly-report').hide('slow');
+        $('#five-day').show('slow');
     });
 });
